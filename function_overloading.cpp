4d17bbd3a1f5a8266ffb395d81d57b859da44c76
kautik <78012455+kautik4@users.noreply.github.com>
2024-10-03 12:19:42 +0530

    Add files via upload

---
+++ function_overloading.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Addition
+{
+   public:
+   int sum(int a, int b)
+   {
+    return a+b;
+   } 
+   int sum(int a,int b, int c)
+   {
+    return a+b+c;
+   }
+   float sum(int a, float b)
+   {
+    return a+b;
+   }
+} ;
+int main(void)
+{
+    Addition obj;
+    cout<<obj.sum(20,15)<<endl;
+    cout<<obj.sum(12,33,45)<<endl;
+    cout<<obj.sum(132,23.44f)<<endl;
+}
+/*
+35
+90
+155.44
+*/ 
